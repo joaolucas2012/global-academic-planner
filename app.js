818ae15b7306fe48dd1f79e13169b80c26e2e832
unknown <ribeirojoaolucas68@gmail.com>
2022-08-31 23:26:09 -0300

    [feat] :rocket: configure mongo db database connection with mongoose

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+// import express
 const express = require("express");
-const app = express();
+// import mongoose
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+// app
+const app = express();
+
+// load env variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log(`DB Connected`));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+// routes
 app.get("/", (req, res) => {
   res.send("Project inicialized");
 });
