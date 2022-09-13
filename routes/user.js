a2c1d3b630dc3d8ef07972a2765892bc53605f7e
unknown <ribeirojoaolucas68@gmail.com>
2022-09-12 22:29:41 -0300

    feat: :rocket: create routes and controllers to the api

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Project inicialized!");
-});
+const { welcome } = require("../controllers/user");
+
+router.get("/", welcome);
 
 module.exports = router;
